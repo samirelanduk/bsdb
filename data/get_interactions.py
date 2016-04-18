b9a7e5851b0e7eb30946a53aa1ae83fcb0964a6e
Sam Ireland <sam.ireland.uk@gmail.com>
2016-04-18 14:57:50 +0100

    Can add rows to interactions table

--- data/get_interactions.py
+++ data/get_interactions.py
@@ -16,4 +16,14 @@ print("There are %i interactions already in the staging database."
 
 interaction_ids_in_table = utilities.get_interaction_ids_from_table(connection)
 
+interactions_added = 0
+for interaction in interactions:
+    if interaction.interaction_id not in interaction_ids_in_table:
+        utilities.add_interaction_to_table(interaction, connection)
+        interactions_added += 1
+print("Added %i rows to the interactions table." % interactions_added)
+print("The interactions table now has %i interactions."
+ % utilities.get_interactions_row_count(connection))
+
+
 connection.close()
--- data/utilities.py
+++ data/utilities.py
@@ -1,4 +1,5 @@
 import psycopg2
+import datetime
 import config
 
 def get_connection():
@@ -25,3 +26,40 @@ def get_interaction_ids_from_table(connection):
     ids = [row[0] for row in cursor.fetchall()]
     cursor.close()
     return ids
+
+
+def add_interaction_to_table(interaction, connection):
+    cursor = connection.cursor()
+    affinity_range = str(interaction.affinity_range[0]
+     ) if len(interaction.affinity_range) == 1 else " - ".join(
+      [str(val) for val in interaction.affinity_range])
+    now = datetime.datetime.now()
+
+    cursor.execute(
+     """INSERT INTO interactions VALUES (
+      %(interactionId)s,
+      %(ligandId)s,
+      %(targetId)s,
+      %(species)s,
+      %(type)s,
+      %(action)s,
+      %(affinityValue)s,
+      %(affinityRange)s,
+      %(ligandIsPeptide)s,
+      %(dateAdded)s,
+      %(dateModified)s
+      );""", {
+       "interactionId": interaction.interaction_id,
+       "ligandId": interaction._ligand_id,
+       "targetId": interaction._target_id,
+       "species": interaction.species,
+       "type": interaction.type,
+       "action": interaction.action,
+       "affinityValue": interaction.affinity_value,
+       "affinityRange":affinity_range,
+       "ligandIsPeptide": "peptide" in interaction.get_ligand().ligand_type.lower(),
+       "dateAdded": now,
+       "dateModified": now
+      }
+    )
+    connection.commit()
