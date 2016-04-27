109dc8e931aad6638810cbaaf7278b6a6ecf6858
Sam Ireland <sam.ireland.uk@gmail.com>
2016-04-27 11:14:15 +0100

    DatabaseAccess now contains SQLExceptions properly (right package this time)

--- manage/WEB-INF/classes/org/bsdbmanage/DatabaseAccess.java
+++ manage/WEB-INF/classes/org/bsdbmanage/DatabaseAccess.java
@@ -8,90 +8,93 @@ import java.io.*;
 public class DatabaseAccess {
 
 	//Returns a connection to the bsdb_stage database
-	public static Connection getConnection() throws SQLException, ClassNotFoundException {
-    Class.forName("org.postgresql.Driver");
-    Connection connection;
-    String url = String.format("jdbc:postgresql://%s/%s", Config.host, Config.db);
-    String user = Config.user;
-    String password = Config.password;
+	public static Connection getConnection() {
+		try {
+	    Class.forName("org.postgresql.Driver");
+	    Connection connection;
+	    String url = String.format("jdbc:postgresql://%s/%s", Config.host, Config.db);
+	    String user = Config.user;
+	    String password = Config.password;
 
-    connection = DriverManager.getConnection(url, user, password);
-    return connection;
+	    connection = DriverManager.getConnection(url, user, password);
+	    return connection;
+		}	catch (SQLException e) {
+			return null;
+ 		} catch (ClassNotFoundException e) {
+			return null;
+ 		}
 	}
 
 
 	//Issues an SQL query to the database and returns the resulting ResultSet
-  public static ResultSet issueRawSqlQuery(String query) throws SQLException, ClassNotFoundException {
+  public static ResultSet issueRawSqlQuery(String query) {
 		Connection conn = getConnection();
-		Statement st = conn.createStatement();
-		ResultSet rs = st.executeQuery(query);
-		return rs;
+		if (conn != null) {
+			try {
+				Statement st = conn.createStatement();
+				ResultSet rs = st.executeQuery(query);
+				return rs;
+			} catch (SQLException e) {
+				return null;
+			}
+		} else {
+			return null;
+		}
 	}
 
 
 	//Takes a ResultSet returned by an SQL query and turns it into an Object array
-  public static Object[][] getObjectGridFromResultSet(ResultSet rs) throws SQLException {
-		int columnCount = rs.getMetaData().getColumnCount();
-		ArrayList<Object[]> rows = new ArrayList<Object[]>();
-		int i = 0;
-		while (rs.next()) {
-			Object[] values = new Object[columnCount];
-			for (int c = 1; c <= columnCount; c++) {
-				values[c-1] = rs.getObject(c);
+  public static Object[][] getObjectGridFromResultSet(ResultSet rs) {
+		try {
+			int columnCount = rs.getMetaData().getColumnCount();
+			ArrayList<Object[]> rows = new ArrayList<Object[]>();
+			int i = 0;
+			while (rs.next()) {
+				Object[] values = new Object[columnCount];
+				for (int c = 1; c <= columnCount; c++) {
+					values[c-1] = rs.getObject(c);
+				}
+				rows.add(values);
+				i++;
 			}
-			rows.add(values);
-			i++;
+	    Object[][] objArray = new Object[rows.size()][columnCount];
+	    objArray = rows.toArray(objArray);
+			return objArray;
+		} catch (SQLException e) {
+			return null;
 		}
-    Object[][] objArray = new Object[rows.size()][columnCount];
-    objArray = rows.toArray(objArray);
-		return objArray;
 	}
 
 
 	//Gets all interactions in the staging database
   public static Interaction[] getAllInteractions() {
-    ArrayList<Interaction> interactions = new ArrayList<Interaction>();
-    try {
-      ResultSet rs = issueRawSqlQuery("SELECT * FROM interactions ORDER BY interactionId;");
-      Object[][] rows = getObjectGridFromResultSet(rs);
+  	ArrayList<Interaction> interactions = new ArrayList<Interaction>();
+    ResultSet rs = issueRawSqlQuery("SELECT * FROM interactions ORDER BY interactionId;");
+		if (rs != null) {
+	    Object[][] rows = getObjectGridFromResultSet(rs);
 
-      for (Object[] row : rows) {
-        interactions.add(new Interaction(row));
-      }
-    } catch (SQLException e) {
-      StringWriter sw = new StringWriter();
-      PrintWriter pw = new PrintWriter(sw);
-      e.printStackTrace(pw);
-      Object[] row = {0, 0, 0, sw.toString(), "", "", "", 0.0, "", false, new Date(), new Date(), new Date()};
-      interactions.add(new Interaction(row));
-    } catch (ClassNotFoundException e) {
-      StringWriter sw = new StringWriter();
-      PrintWriter pw = new PrintWriter(sw);
-      e.printStackTrace(pw);
-      Object[] row = {0, 0, 0, sw.toString(), "", "", "", 0.0, "", false, new Date(), new Date(), new Date()};
-      interactions.add(new Interaction(row));
-    }
-    Interaction[] interactionsArray = new Interaction[interactions.size()];
-    interactionsArray = interactions.toArray(interactionsArray);
-    return interactionsArray;
+	    for (Object[] row : rows) {
+	      interactions.add(new Interaction(row));
+	    }
+	    Interaction[] interactionsArray = new Interaction[interactions.size()];
+	    interactionsArray = interactions.toArray(interactionsArray);
+	    return interactionsArray;
+		} else {
+			return null;
+		}
   }
 
 
 	public static ArrayList<String> getPdbsOfInteraction(int interactionId) {
 		ArrayList<String> pdbs = new ArrayList<String>();
-		try {
-			ResultSet rs = issueRawSqlQuery(String.format(
-			 "SELECT pdbCode FROM interaction_pdbs WHERE interactionId=%d", interactionId
-			));
-			Object[][] rows = getObjectGridFromResultSet(rs);
-			for (Object[] row : rows) {
-        pdbs.add((String)row[0]);
-      }
-		} catch (SQLException e) {
+		ResultSet rs = issueRawSqlQuery(String.format(
+		 "SELECT pdbCode FROM interaction_pdbs WHERE interactionId=%d", interactionId
+		));
+		Object[][] rows = getObjectGridFromResultSet(rs);
+		for (Object[] row : rows) {
+      pdbs.add((String)row[0]);
+    }
 
-		} catch (ClassNotFoundException e) {
-
-		}
 		return pdbs;
 	}
 
