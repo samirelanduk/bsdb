57e7a957374a984d71d0ab77c671f58fa10fdfa3
Sam Ireland <sam.ireland.uk@gmail.com>
2016-05-19 17:48:29 +0100

    Start bindsite searching

--- data/identify_bind_sites.py
+++ data/identify_bind_sites.py
@@ -15,6 +15,14 @@ try:
     print("There are %i Interaction-PDB maps with no binding site." % len(
      interaction_pdb_maps
     ))
+
+    print("Looking for binding sites...")
+    for pdb_map in interaction_pdb_maps:
+        pdb = molecupy.get_pdb_remotely(pdb_map[2])
+        ligand = pdb.model.get_small_molecule_by_id(pdb_map[3])
+        print("\t%i%s: Looking for %s's binding site in PDB %s..." % (
+         pdb_map[1], pdb_map[2], ligand, pdb_map[2]
+        ), end=" ")
 finally:
     connection.close()
     print("")
