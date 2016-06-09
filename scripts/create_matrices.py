4084e47cd8396ab60ab2559c3a2a43b2e12422b5
Sam Ireland <sam.ireland.uk@gmail.com>
2016-06-09 16:16:53 +0100

    Matrix creator can get existing matrices

--- scripts/create_matrices.py
+++ scripts/create_matrices.py
@@ -1,5 +1,6 @@
 import utilities
 import sys
+import os
 print("")
 
 connection = utilities.get_live_connection()
@@ -7,6 +8,12 @@ connection = utilities.get_live_connection()
 try:
     sequence_ids = utilities.get_sequence_ids_from_table(connection)
     print("There are %i sequences in the live database." % len(sequence_ids))
+
+    paths = utilities.get_paths("bsdb")
+    matrices = os.listdir(paths["tomcat_dir"] + "/static/matrices")
+    matrix_ids = [int(f.split(".")[0]) for f in matrices if f[0] != "."]
+    sequences_without_matrix = [i for i in sequence_ids if i not in matrix_ids]
+    print("%i of these need a matrix generating." % len(sequences_without_matrix))
 finally:
     connection.close()
     print("")
