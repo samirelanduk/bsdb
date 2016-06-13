02dd4b34d512ede1a492c5aca155c484e1d2d550
Sam Ireland <sam.ireland.uk@gmail.com>
2016-06-13 14:19:30 +0100

    Small fixes

--- scripts/identify_sequences.py
+++ scripts/identify_sequences.py
@@ -9,8 +9,10 @@ try:
     interaction_pdb_maps = utilities.get_interaction_pdb_maps(connection)
     print("There are %i Interaction-PDB maps." % len(interaction_pdb_maps))
 
+
+
     interaction_pdb_maps = [
-     pdb_map for pdb_map in interaction_pdb_maps if not pdb_map["bindSequence"]
+     pdb_map for pdb_map in interaction_pdb_maps if not pdb_map["bindSequence"] and pdb_map["bindingResidues"]
     ]
     print("There are %i Interaction-PDB maps with no bind sequence." % len(
      interaction_pdb_maps
@@ -23,29 +25,30 @@ try:
         print("\t%i%s: Looking for %s's bind sequence in PDB %s..." % (
          pdb_map["interactionId"], pdb_map["pdbCode"], ligand, pdb_map["pdbCode"]
         ), end=" ")
-        residue_ids = pdb_map["bindingResidues"].split(", ")
-        residues = []
-        for residue_id in residue_ids:
-            if residue_id[0].isalpha():
-                chain = pdb.model.get_chain_by_id(residue_id[0])
+        binding_residue_ids = pdb_map["bindingResidues"].split(", ")
+        binding_residues = []
+        for binding_residue_id in binding_residue_ids:
+            if binding_residue_id[0].isalpha():
+                chain = pdb.model.get_chain_by_id(binding_residue_id[0])
                 if chain and chain.residues:
-                    residue = chain.get_residue_by_id(residue_id)
+                    residue = chain.get_residue_by_id(binding_residue_id)
                     if residue:
-                        residues.append(residue)
-        if len(set([residue.chain.chain_id for residue in residues])) == 1:
-            chain = residues[0].chain
-            residues = sorted(residues, key=lambda k: chain.residues.index(k))
-            sequence_residues = [residues[0]]
+                        binding_residues.append(residue)
+        if len(set([binding_residue.chain.chain_id for binding_residue in binding_residues])) == 1: # Esure all residues on same chain
+            chain = binding_residues[0].chain
+            binding_residues = sorted(binding_residues, key=lambda k: chain.residues.index(k))
+            print([r.residue_id for r in binding_residues])
+            sequence_residues = [binding_residues[0]]
             while sequence_residues[-1].downstream_residue:
                 sequence_residues.append(sequence_residues[-1].downstream_residue)
-                if sequence_residues[-1] is residues[-1]:
+                if sequence_residues[-1] is binding_residues[-1]:
                     break
-            if residues[-1] is not sequence_residues[-1]:
+            if binding_residues[-1] is not sequence_residues[-1]:
                 print("Can't - sequence is missing residues")
             else:
                 sequence = ResiduicSequence(*sequence_residues)
                 sequence_string = "".join([
-                 RESIDUES.get(res.residue_name, "x") if res in residues else
+                 RESIDUES.get(res.residue_name, "x") if res in binding_residues else
                   RESIDUES.get(res.residue_name, "x").lower() for res in sequence.residues
                 ])
                 chain_length = len(chain.residues) + len(chain.missing_residues)
