a221c32c067c7bc5698a511db068ac6958b4a18f
Sam Ireland <sam.ireland.uk@gmail.com>
2016-08-09 17:15:41 +0100

    Add chain identifier in insertion script

--- insertions/insertion.py
+++ insertions/insertion.py
@@ -14,6 +14,15 @@ except molecupy.InvalidPdbCodeError:
     print("%s doesn't seem to be a valid PDB code.\n" % sys.argv[1])
     sys.exit()
 
-
-
+# Get chain
+chain = None
+try:
+    chain = sys.argv[2]
+    chain = pdb.model().get_chain_by_id(chain)
+except IndexError:
+    print("Please provide a chain ID.\n")
+    sys.exit()
+if chain is None:
+    print("%s doesn't seem to be a valid chain in PDB %s\n" % (sys.argv[2], pdb.pdb_code()))
+    sys.exit()
 print("")
