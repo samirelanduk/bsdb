d5be4a22492c3f0c396acfe39be113f69ef839c0
Sam Ireland <sam.ireland.uk@gmail.com>
2016-08-09 17:12:07 +0100

    Add PDB acquisition to insertion script

--- insertions/insertion.py
+++ insertions/insertion.py
@@ -1,2 +1,19 @@
 import molecupy
 import sys
+print("")
+
+# Get PDB
+pdb = None
+try:
+    code = sys.argv[1]
+    pdb = molecupy.get_pdb_remotely(code)
+except IndexError:
+    print("Please provide a PDB code.\n")
+    sys.exit()
+except molecupy.InvalidPdbCodeError:
+    print("%s doesn't seem to be a valid PDB code.\n" % sys.argv[1])
+    sys.exit()
+
+
+
+print("")
