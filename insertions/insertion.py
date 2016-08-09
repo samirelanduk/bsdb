1133f4697ca6ef5bd5584187252afd40a03d07f5
Sam Ireland <sam.ireland.uk@gmail.com>
2016-08-09 19:30:36 +0100

    Add contact counter to insertions script

--- insertions/insertion.py
+++ insertions/insertion.py
@@ -1,4 +1,5 @@
 import molecupy
+from molecupy.structures import ResiduicSequence
 import sys
 print("")
 
@@ -34,4 +35,17 @@ print("It has %i residues." % len(chain.residues()))
 print("%i of these are missing from the PDB structure." % (
  len(chain.residues()) - len(chain.residues(include_missing=False)))
 )
+
+# Go through each possible cut
+for index, residue in enumerate(chain.residues()[:-1]):
+    next_residue = chain.residues()[index + 1]
+    sequence1 = ResiduicSequence(*chain.residues()[:index + 1])
+    sequence2 = ResiduicSequence(*chain.residues()[index:])
+    usable1 = len(sequence1.residues(include_missing=False)) / len(sequence1.residues()) >= 0.5
+    usable2 = len(sequence2.residues(include_missing=False)) / len(sequence2.residues()) >= 0.5
+    if usable1 and usable2:
+        internal1 = len(sequence1.internal_contacts())
+        external = len(sequence1.contacts_with(sequence2))
+        internal2 = len(sequence2.internal_contacts())
+        print("\t%i: %s-%s (%.1f/%.1f)" % (index + 1, residue.residue_name(), next_residue.residue_name(), internal1/external, internal2/external))
 print("")
