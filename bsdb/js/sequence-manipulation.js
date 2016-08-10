bbb759ad23ef1d26cb60b3be92ada84d6504fdb1
Sam Ireland <sam.ireland.uk@gmail.com>
2016-08-10 11:31:04 +0100

    Add sequence left extender

--- bsdb/js/sequence-manipulation.js
+++ bsdb/js/sequence-manipulation.js
@@ -1,5 +1,11 @@
 function leftout() {
-
+  var sequence = document.getElementById("sequence");
+  for (var i = 0; i < sequence.children.length; i++) {
+    if ((sequence.children[i].className.includes("inside")) && (i != 0)) {
+      sequence.children[i - 1].className = "between inside";
+      break;
+    }
+  }
 }
 
 function leftin() {
