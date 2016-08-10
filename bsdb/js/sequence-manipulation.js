d474e4ba8464687d78b39906b64022731239e324
Sam Ireland <sam.ireland.uk@gmail.com>
2016-08-10 11:32:51 +0100

    Add sequence right extender

--- bsdb/js/sequence-manipulation.js
+++ bsdb/js/sequence-manipulation.js
@@ -21,5 +21,11 @@ function rightin() {
 }
 
 function rightout() {
-
+  var sequence = document.getElementById("sequence");
+  for (var i = sequence.children.length - 1; i > 0; i--) {
+    if ((sequence.children[i].className.includes("inside")) && (i != sequence.children.length - 1)) {
+      sequence.children[i + 1].className = "between inside";
+      break;
+    }
+  }
 }
