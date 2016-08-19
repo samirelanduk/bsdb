a17f7063e6771091bf416a24e45275c3cca7021d
Sam Ireland <sam.ireland.uk@gmail.com>
2016-08-19 19:16:46 +0100

    Add handling of antibody ligands in bindsite finder

--- scripts/identify_bind_sites.py
+++ scripts/identify_bind_sites.py
@@ -1,4 +1,5 @@
 import molecupy
+from molecupy.structures import ResiduicStructure
 import utilities
 import warnings
 print("")
@@ -22,13 +23,23 @@ try:
         try:
             pdb = molecupy.get_pdb_remotely(pdb_map["pdbCode"])
             ligand_is_peptide = len(pdb_map["hetId"]) == 1
-            ligand = pdb.model().get_chain_by_id(pdb_map["hetId"]) if ligand_is_peptide\
-             else pdb.model().get_small_molecule_by_id(pdb_map["hetId"])
+            ligand_is_antibody = "," in pdb_map["hetId"]
+            ligand = None
+            if ligand_is_peptide:
+                ligand = pdb.model().get_chain_by_id(pdb_map["hetId"])
+            elif ligand_is_antibody:
+                chains = [pdb.model().get_chain_by_id(id_) for id_ in pdb_map["hetId"].split(",")]
+                residues = []
+                for chain in chains:
+                    residues += chain.residues()
+                ligand = ResiduicStructure(*residues)
+            else:
+                ligand = pdb.model().get_small_molecule_by_id(pdb_map["hetId"])
             print("\t%i%s: Looking for %s's binding site in PDB %s..." % (
              pdb_map["interactionId"], pdb_map["pdbCode"], ligand, pdb_map["pdbCode"]
             ), end=" ")
             if ligand:
-                site = ligand.bind_site() if not ligand_is_peptide else None
+                site = ligand.bind_site() if not ligand_is_peptide and not ligand_is_antibody else None
                 calculation_required = False
                 if not site:
                     calculation_required = True
